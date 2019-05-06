06d27fec1a431b532f165f3a8c074140779aa070
chy <983039901@qq.com>
2019-05-06 16:05:07 +0800

    加入redis缓存

--- webchatserver/src/main/java/com/chy/webchatserver/WebchatserverApplication.java
+++ webchatserver/src/main/java/com/chy/webchatserver/WebchatserverApplication.java
@@ -3,9 +3,11 @@ package com.chy.webchatserver;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
 @MapperScan("com.chy.webchatserver.mapper")
+@EnableCaching
 public class WebchatserverApplication {
 
     public static void main(String[] args) {
--- webchatserver/src/main/java/com/chy/webchatserver/service/MenuService.java
+++ webchatserver/src/main/java/com/chy/webchatserver/service/MenuService.java
@@ -3,16 +3,20 @@ package com.chy.webchatserver.service;
 import com.chy.webchatserver.entity.Menu;
 import com.chy.webchatserver.mapper.MenuMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import javax.annotation.Resource;
 import java.util.List;
 
 @Service
+@Transactional
 public class MenuService {
     @Resource
     MenuMapper menuMapper;
 
+    @Cacheable(key = "#root.methodName")
     public List<Menu> getAllMenu(){
         return menuMapper.getAllMenu();
     }
