246bd2fb0f828fb3bb6af653635e0423ca77268a
chy <983039901@qq.com>
2019-05-06 21:14:27 +0800

    在线聊天ing，明天写前端界面

---
+++ webchatserver/src/main/java/com/chy/webchatserver/entity/ChatResp.java
@@ -0,0 +1,29 @@
+package com.chy.webchatserver.entity;
+
+public class ChatResp {
+    private String msg;
+    private String from;
+
+    public ChatResp(){}
+
+    public ChatResp(String msg, String from) {
+        this.msg = msg;
+        this.from = from;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+
+    public String getFrom() {
+        return from;
+    }
+
+    public void setFrom(String from) {
+        this.from = from;
+    }
+}
